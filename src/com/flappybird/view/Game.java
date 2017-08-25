187b1a7e71dbbf99e6eb34f1a78de8adc05efc78
derickfelix <derickfelix4321@gmail.com>
2017-08-25 09:45:00 -0400

    Game, and Window

---
+++ src/com/flappybird/view/Game.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flappybird.view;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author derickfelix
+ */
+public class Game extends JPanel{
+    
+}
---
+++ src/com/flappybird/view/Window.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.flappybird.view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author derickfelix
+ */
+public class Window {
+    public Window(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame();
+        
+        frame.add(game);
+        frame.setTitle(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Game game = new Game();
+        
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ContainerDeJanelas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            
+            Window window = new Window(800, 600, "Flappy Bird", game);
+            
+        });
+    }
+}
